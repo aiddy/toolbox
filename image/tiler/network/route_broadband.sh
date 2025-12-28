ad28bf912632498c8986792c626e7577377f002c
Aiddy <Aiddy@aiddy.com>
2025-12-28 09:24:54 +0000

    fix directory structure

--- image/tiler/network/route_broadband.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# route_broadband.sh -- Configure default route for broadband network which is on wlan0 on pi 
-sudo ip route del default
-sudo ip route add default via 192.168.0.1 dev wlan0
--- image/tiler/network/set_exit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# set_exit.sh - set tailscale exit node
-tailscale set --exit-node=192.168.0.187
